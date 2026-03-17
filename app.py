dc811c6cd8ab0d178894dccd963a77cb794c129c
Fortune <fortchimez@gmail.com>
2026-03-17 16:09:43 +0100

    updated vercel deployment requirements

--- app.py
+++ app.py
@@ -66,4 +66,7 @@ app = create_app()
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True, port=5000)
+    # Port 5000 is used locally. Vercel automatically assigns a port.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
+
